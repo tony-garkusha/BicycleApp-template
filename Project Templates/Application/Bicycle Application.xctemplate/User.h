0b1fd06bf5cabe56dc421346c846648a3c2e4e45
tony-garkusha <ant.garkusha@gmail.com>
2012-11-20 16:56:36 +0200

    #4 Add Basic User model class

---
+++ Project Templates/Application/Bicycle Application.xctemplate/User.h
@@ -0,0 +1,16 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ___VARIABLE_classPrefix:identifier___User : NSObject
+
+@property (nonatomic, copy) NSString *username;
+@property (nonatomic, copy) NSString *password;
+
+@end
---
+++ Project Templates/Application/Bicycle Application.xctemplate/User.m
@@ -0,0 +1,13 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+#import "___VARIABLE_classPrefix:identifier___User.h"
+
+@implementation ___VARIABLE_classPrefix:identifier___User
+
+@end
